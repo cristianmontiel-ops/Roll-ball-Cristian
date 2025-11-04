b377fbcaa5d1fe472d549bf362018bc5f381951d
cristianmontiel-ops <cristian.montiel@discoverystudent.org>
2025-11-04 11:30:33 -0500

    Script

--- Assets/scripts/player_controller.cs
+++ Assets/scripts/player_controller.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
-using UnityEngine. InputSystem;
+using UnityEngine.InputSystem;
+
 public class player_controller : MonoBehaviour
 {
+   
+private Rigidbody rb;
+
+private float movementX;
+private float movementY; 
+
+public float speed = 0; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+       
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+    Vector2 movementVector = movementValue.Get<Vector2>();
+    movementX = movementVector.x;
+    movementY = movementVector.y;
     }
 }
- Void Onmove(InputValue movementValue)
